654b80d5570876fd7add42f024cefc282075a9f1
wisecodeworks <taterdev@gmail.com>
2019-07-08 15:23:22 -0400

    file updated for the footer widgets

--- functions.php
+++ functions.php
@@ -115,6 +115,24 @@ function bootscores_widgets_init() {
 	'before_title' => '<h3 class="widget-title">',
 	'after_title' => '</h3>',
 	) );
+	register_sidebar( array(
+	'name' => 'Footer Sidebar 2',
+	'id' => 'footer-sidebar-2',
+	'description' => 'Appears in the footer area',
+	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	'after_widget' => '</aside>',
+	'before_title' => '<h3 class="widget-title">',
+	'after_title' => '</h3>',
+	) );
+	register_sidebar( array(
+	'name' => 'Footer Sidebar 3',
+	'id' => 'footer-sidebar-3',
+	'description' => 'Appears in the footer area',
+	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	'after_widget' => '</aside>',
+	'before_title' => '<h3 class="widget-title">',
+	'after_title' => '</h3>',
+	) );
 }
 
 add_action( 'widgets_init', 'bootscores_widgets_init' );
